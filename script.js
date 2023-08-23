f0d588308018a07d7f52dc4d2eddf38030cf5421
Caterina <137883905+Caterina87@users.noreply.github.com>
2023-08-23 17:15:57 +0200

    add changes

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-fetch("https://striveschool-api.herokuapp.com/books").then((response) => response.json());
-let booksObj = [];
+fetch("https:striveschool-api.herokuapp.com/books")
+  .then((response) => response.json())
+  .then((booksObj) => {
+    console.log(booksObj);
+    const row = document.querySelector("row");
+
+    booksObj.response.forEach((book) => {
+      const title = book.title;
+
+      const col = document.createElement("div");
+      col.className = "col";
+
+      col.innerHTML = `<div class="card">
+                                         <img src=${book.img} class="card-img-top" alt="...">
+                                         <div class="card-body">
+                                             <h5 class="card-title">${book.title}</h5>
+                                             <p class="card-text">${book.price}</p>
+                                             <a href="#" class="btn btn-primary">Add to Cart</a>
+                                             <a href="#" class="btn btn-primary">Remove</a>
+                                         </div>
+                                     </div>`;
+
+      row.appendChild(col);
+    });
+  })
+  .catch((error) => console.log(error));
